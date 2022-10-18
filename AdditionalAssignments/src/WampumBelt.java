457c19e43e12631e136531786f4b8f6c713c8f08
1005096004 <1005096004@L815WD220024.edu.cbe.ab.ca>
2022-10-18 15:31:30 -0600

    10/18/2022 - 3:31PM

---
+++ AdditionalAssignments/src/WampumBelt.java
@@ -0,0 +1,90 @@
+
+public class WampumBelt 
+{
+	public static int beltLength = 12;
+	
+	public static boolean tasslesDrawn = false;
+
+	public static void main(String[] args) 
+	{
+		Tassles();
+		Border();
+		Triangle();
+		Tassles();
+	}
+	
+	static void Tassles()
+	{
+		if(tasslesDrawn)
+		{
+			for(int i = 0; i < beltLength; i++)
+			{
+				System.out.print("--");
+			}
+			
+			System.out.println("");
+		}
+		
+		for(int i = 0; i < beltLength; i++)
+		{
+			System.out.print("| ");
+		}
+		
+		System.out.println("");
+		
+		for(int i = 0; i < beltLength; i++)
+		{
+			System.out.print("| ");
+		}
+		
+		System.out.println("");
+		
+		if(!tasslesDrawn)
+		{
+			for(int i = 0; i < beltLength; i++)
+			{
+				System.out.print("--");
+			}
+		}
+		
+		System.out.println("");
+		
+		tasslesDrawn = true;
+	}
+	
+	static void Border()
+	{
+		int spaceBetween = beltLength - 2;
+		System.out.print("||");
+		for(int i = 0; i < spaceBetween; i++)
+		{
+			System.out.print("  ");
+		}
+		System.out.print("||");
+		System.out.println("");
+		return;
+	}
+	
+	//Design Methods
+	
+	static void Triangle()
+	{
+		System.out.print("||         /\\         ||");
+		System.out.println("");
+		System.out.print("||        /  \\        ||");
+		System.out.println("");
+		System.out.print("||       /    \\       ||");
+		System.out.println("");
+		System.out.print("||      /      \\      ||");
+		System.out.println("");
+		System.out.print("||      \\      /      ||");
+		System.out.println("");
+		System.out.print("||       \\    /       ||");
+		System.out.println("");
+		System.out.print("||        \\  /        ||");
+		System.out.println("");
+		System.out.print("||         \\/         ||");
+		System.out.println("");
+	}
+
+}
